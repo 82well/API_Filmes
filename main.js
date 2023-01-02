6eb288b68b19354992f97d62bc89929b594f5b14
82well <nylook2@hotmail.com>
2023-01-02 19:57:17 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const baseUrl = "https://api.themoviedb.org/3";
+const imageUrl = "https://image.tmdb.org/t/p/w780";
+const apiKey = "5cd249d4f26bf03481d34df30898c03e";
+
+const movieList = document.querySelector("#movie-list");
